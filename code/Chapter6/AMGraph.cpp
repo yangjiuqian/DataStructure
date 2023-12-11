0e048b1148c4972136a567c2e72e0962c0328280
yangjiuqian <425750540@qq.com>
2023-12-11 17:46:21 +0800

    12-11-2

--- code/Chapter6/AMGraph.cpp
+++ code/Chapter6/AMGraph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -7,10 +8,16 @@ class Graph {
 private:
     int numVertices;
     vector<vector<int>> adjacencyMatrix;
+    unordered_map<int, string> vertexTable;  // 顶点表，将顶点的标识映射到实际的顶点名称
 
 public:
-    // 构造函数，初始化图的顶点数和邻接矩阵
+    // 构造函数，初始化图的顶点数、邻接矩阵和顶点表
     Graph(int vertices) : numVertices(vertices), adjacencyMatrix(vertices, vector<int>(vertices, 0)) {}
+
+    // 添加顶点，将顶点标识映射到实际的顶点名称
+    void addVertex(int id, const string& name) {
+        vertexTable[id] = name;
+    }
 
     // 添加边
     void addEdge(int src, int dest) {
@@ -21,6 +28,14 @@ public:
 
     // 打印邻接矩阵
     void printMatrix() {
+        // 打印顶点表
+        cout << "Vertex Table:" << endl;
+        for (const auto& entry : vertexTable) {
+            cout << entry.first << ": " << entry.second << endl;
+        }
+
+        // 打印邻接矩阵
+        cout << "Adjacency Matrix:" << endl;
         for (int i = 0; i < numVertices; ++i) {
             for (int j = 0; j < numVertices; ++j) {
                 cout << adjacencyMatrix[i][j] << " ";
@@ -31,17 +46,22 @@ public:
 };
 
 int main() {
-    // 创建一个包含5个顶点的图
-    Graph graph(5);
+    // 创建一个包含4个顶点的图
+    Graph graph(4);
+
+    // 添加顶点
+    graph.addVertex(0, "A");
+    graph.addVertex(1, "B");
+    graph.addVertex(2, "C");
+    graph.addVertex(3, "D");
 
     // 添加边
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
     graph.addEdge(1, 3);
-    graph.addEdge(2, 4);
+    graph.addEdge(2, 3);
 
-    // 打印邻接矩阵
-    cout << "Adjacency Matrix:" << endl;
+    // 打印顶点表和邻接矩阵
     graph.printMatrix();
 
     return 0;
